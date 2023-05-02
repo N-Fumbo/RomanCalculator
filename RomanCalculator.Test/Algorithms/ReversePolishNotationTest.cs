10dd625bb9425aa5af4f4b4d2eda240fa22d25af
Alexander <74.71.spartacus@gmail.com>
2023-05-02 15:50:53 +0300

    Реализация алгоритма польской обратной связи

--- RomanCalculator.Test/Algorithms/ReversePolishNotationTest.cs
+++ RomanCalculator.Test/Algorithms/ReversePolishNotationTest.cs
@@ -1,9 +1,4 @@
 ﻿using RomanCalculator.Algorithms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RomanCalculator.Test.Algorithms
 {
@@ -11,11 +6,12 @@ namespace RomanCalculator.Test.Algorithms
     {
         [Theory]
         [InlineData("3 + 4", "3 4 +")]
+        [InlineData("( 1 + 2 ) * ( 3 + 4 )", "1 2 + 3 4 + *")]
         [InlineData("2 * ( 3 + 4 )", "2 3 4 + *")]
         [InlineData("4 + 5 * 3", "4 5 3 * +")]
-        [InlineData("( 7 - 2 ) / ( 3 + 2 )", "7 2 - 3 2 + /")]
+        [InlineData("( 7 - 2 ) * ( 3 + 2 )", "7 2 - 3 2 + *")]
         [InlineData("2 + 3 * 4 - 5", "2 3 4 * + 5 -")]
-        [InlineData("( 1 + 2 ) * ( 3 + 4 ) / ( 5 + 6 )", "1 2 + 3 4 + * 5 6 + *")]
+        [InlineData("( 1 + 2 ) * ( 3 + 4 ) * ( 5 + 6 )", "1 2 + 3 4 + * 5 6 + *")]
         public void ToRoman_ConvertsCorrectly(string expresion, string expected)
         {
             List<string> actual = new ReversePolishNotation().ConvertToPostfix(expresion);
--- RomanCalculator/Algorithms/ReversePolishNotation.cs
+++ RomanCalculator/Algorithms/ReversePolishNotation.cs
@@ -1,16 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace RomanCalculator.Algorithms
 {
     public class ReversePolishNotation
     {
+        
+        private static readonly char[] _operators = { '+', '-', '*' };
+
         public List<string> ConvertToPostfix(string expression)
         {
-            throw new NotImplementedException();
+            List<string> postfix = new();
+            Stack<char> operators = new();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                if (char.IsDigit(c))
+                {
+                    StringBuilder number = new();
+                    while (char.IsDigit(c))
+                    {
+                        number.Append(c);
+                        i++;
+
+                        if (i < expression.Length) c = expression[i];
+                        else break;
+                    }
+
+                    i--;
+                    postfix.Add(number.ToString());
+                }
+                else if (_operators.Contains(c))
+                {
+                    while (operators.Count > 0 && GetPriorityOperation(c) <= GetPriorityOperation(operators.Peek()))
+                        postfix.Add(operators.Pop().ToString());
+
+                    operators.Push(c);
+                }
+                else if (c == '(')
+                {
+                    operators.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (operators.Peek() != '(')
+                        postfix.Add(operators.Pop().ToString());
+
+                    operators.Pop();
+                }
+            }
+
+            while (operators.Count > 0)
+                postfix.Add(operators.Pop().ToString());
+
+            return postfix;
+        }
+
+        private static int GetPriorityOperation(char opertaion)
+        {
+            if (opertaion == '(' || opertaion == ')')
+                return 0;
+            else if (opertaion == '+' || opertaion == '-')
+                return 1;
+            else if (opertaion == '*')
+                return 2;
+
+            throw new ArgumentException($"Неизвестный оператор для оценки приоритета. {opertaion}", nameof(opertaion));
         }
     }
 }
