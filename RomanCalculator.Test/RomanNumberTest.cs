2553f7747fa0e670a82b750f135a99fed285cea5
Alexander <74.71.spartacus@gmail.com>
2023-05-02 12:41:16 +0300

    Реализация метода конвертации из римского числа в арабское + написание доп. тестов для этого метода.

--- RomanCalculator.Test/RomanNumberTest.cs
+++ RomanCalculator.Test/RomanNumberTest.cs
@@ -11,10 +11,37 @@
         [InlineData("CD", 400)]
         [InlineData("CM", 900)]
         [InlineData("MV", 1005)]
-        public void ToArabic_ConvertsCorrectly(string roman, int expected)
+        [InlineData("MCMLIV", 1954)]
+        [InlineData("MMXIV", 2014)]
+        [InlineData("MCMXCIX", 1999)]
+        [InlineData("MMMCMXCIX", 3999)]
+        [InlineData(" ", null)]
+        [InlineData("IVVVV", null)]
+        [InlineData("MMMIM", null)]
+        [InlineData("VIV", null)]
+        [InlineData("CCCC", null)]
+        [InlineData("XCD", null)]
+        [InlineData("IIIX", null)]
+        [InlineData("VX", null)]
+        [InlineData("IL", null)]
+        [InlineData("CDM", null)]
+        [InlineData("MCMC", null)]
+        [InlineData("LXL", null)]
+        public void ToArabic_ConvertsCorrectly(string roman, int? expected)
         {
-            var actual = new RomanNumber().ConvertToArabic(roman);
-            Assert.Equal(expected, actual);
+            if (string.IsNullOrWhiteSpace(roman))
+            {
+                Assert.ThrowsAny<ArgumentNullException>(() => new RomanNumber().ConvertToArabic(roman));
+            }
+            if (expected.HasValue)
+            {
+                var actual = new RomanNumber().ConvertToArabic(roman);
+                Assert.Equal(expected, actual);
+            }
+            else
+            {
+                Assert.ThrowsAny<ArgumentException>(() => new RomanNumber().ConvertToArabic(roman));
+            }
         }
     }
 }
--- RomanCalculator/RomanNumber.cs
+++ RomanCalculator/RomanNumber.cs
@@ -1,15 +1,45 @@
-﻿namespace RomanCalculator
+﻿using System.Text.RegularExpressions;
+
+namespace RomanCalculator
 {
-    public class RomanNumber
+    public partial class RomanNumber
     {
+        private static readonly Dictionary<char, int> _romanInArabicMap = new()
+        {
+            ['I'] = 1,
+            ['V'] = 5,
+            ['X'] = 10,
+            ['L'] = 50,
+            ['C'] = 100,
+            ['D'] = 500,
+            ['M'] = 1000
+        };
+
         public int ConvertToArabic(string roman)
         {
-            throw new NotImplementedException();
+            if(string.IsNullOrWhiteSpace(roman)) throw new ArgumentNullException(nameof(roman));
+
+            if(CheckRomanNumberRegex().IsMatch(roman) is false) throw new ArgumentException("Недопустимое значение римского числа.", nameof(roman));
+
+            int result = 0;
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                int current = _romanInArabicMap[roman[i]];
+
+                result += i + 1 < roman.Length && _romanInArabicMap[roman[i + 1]] > current
+                    ? -current : current;
+            }
+
+            return result;
         }
 
         public string ConvertToRoman(int arabic)
         {
             throw new NotImplementedException();
         }
+
+        [GeneratedRegex("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")]
+        private static partial Regex CheckRomanNumberRegex();
     }
 }
