5ae7be12fe679f840b3a586efd9dff4fb30f16b6
Alexander <74.71.spartacus@gmail.com>
2023-05-02 13:29:23 +0300

    Создание класса алгоритма обратной польской запись, написание тестов для этого класса

---
+++ RomanCalculator.Test/Algorithms/ReversePolishNotationTest.cs
@@ -0,0 +1,26 @@
+﻿using RomanCalculator.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanCalculator.Test.Algorithms
+{
+    public class ReversePolishNotationTest
+    {
+        [Theory]
+        [InlineData("3 + 4", "3 4 +")]
+        [InlineData("2 * ( 3 + 4 )", "2 3 4 + *")]
+        [InlineData("4 + 5 * 3", "4 5 3 * +")]
+        [InlineData("( 7 - 2 ) / ( 3 + 2 )", "7 2 - 3 2 + /")]
+        [InlineData("2 + 3 * 4 - 5", "2 3 4 * + 5 -")]
+        [InlineData("( 1 + 2 ) * ( 3 + 4 ) / ( 5 + 6 )", "1 2 + 3 4 + * 5 6 + *")]
+        public void ToRoman_ConvertsCorrectly(string expresion, string expected)
+        {
+            List<string> actual = new ReversePolishNotation().ConvertToPostfix(expresion);
+            string result = string.Join(" ", actual);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ RomanCalculator/Algorithms/ReversePolishNotation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanCalculator.Algorithms
+{
+    public class ReversePolishNotation
+    {
+        public List<string> ConvertToPostfix(string expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
