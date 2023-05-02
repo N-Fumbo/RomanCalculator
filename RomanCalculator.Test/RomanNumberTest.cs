c1078a936a4d2e6ba14ba60e64e46e75f1d1eb0c
Alexander <74.71.spartacus@gmail.com>
2023-05-02 12:25:31 +0300

    Создан класс для конвератции римского числа в арабскогое и наоборот. Написание тестов на метода конвертации из римского в арабское.

---
+++ RomanCalculator.Test/RomanNumberTest.cs
@@ -0,0 +1,20 @@
+﻿namespace RomanCalculator.Test
+{
+    public class RomanNumberTest
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("IV", 4)]
+        [InlineData("IX", 9)]
+        [InlineData("XL", 40)]
+        [InlineData("XC", 90)]
+        [InlineData("CD", 400)]
+        [InlineData("CM", 900)]
+        [InlineData("MV", 1005)]
+        public void ToArabic_ConvertsCorrectly(string roman, int expected)
+        {
+            var actual = new RomanNumber().ConvertToArabic(roman);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ RomanCalculator/RomanNumber.cs
@@ -0,0 +1,15 @@
+﻿namespace RomanCalculator
+{
+    public class RomanNumber
+    {
+        public int ConvertToArabic(string roman)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertToRoman(int arabic)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
